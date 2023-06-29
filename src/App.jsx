42b41d6cfe7d9e9df2e2f8aab858991aba8567fc
avi <avikhajuria1994@outlook.com>
2023-06-29 08:37:57 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Poetry } from './components/Poetry'
 
 const App = () => (
       <Routes>
-        <Route path="/Poetry_Project" element={<Home />}/>
+        <Route path="/Poetry_Project/Home" element={<Home />}/>
         <Route path="Poetry_Project/Stories" element={<Stories />}/>
         <Route path="Poetry_Project/Poetry" element={<Poetry />} />
       </Routes>  
