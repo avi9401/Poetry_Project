cb13154528aca212f1cac709d8730e4cf4c680d8
avi <avikhajuria1994@outlook.com>
2023-06-29 08:47:37 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import {Navbar, Hero, Links, Home} from './components';
-import { Stories } from './components/Stories';
-import { Poetry } from './components/Poetry';
-import { Home } from './components/Home';
+import { Stories } from './components/Stories'
+import { Poetry } from './components/Poetry'
 
 
 const App = () => (
       <Routes>
         <Route path="/Poetry_Project" element={<Home />}/>
-        <Route path="Poetry_Project/Stories" element={<Stories />}/>
-        <Route path="Poetry_Project/Poetry" element={<Poetry />} />
+        <Route path="/Poetry_Project/Stories" element={<Stories />}/>
+        <Route path="/Poetry_Project/Poetry" element={<Poetry />} />
       </Routes>  
   )
 
