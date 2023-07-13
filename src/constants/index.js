8ad415763d4647f01070ea6202060912b885c47b
avi <avikhajuria1994@outlook.com>
2023-07-13 23:36:19 +0530

    uploading content

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,34 +2,137 @@ export const stories = [
   {
 
     id: "1",
-    title: "User Active",
-    story:  ` in Jammu: the city of temples, there is a house.
-      On the other side of Tawi, past the old bridge, i sit in my memory;
-      she's talking to me, "earn so i can be free," as my heart drowns in summer.
-      "it's unbearable," i say -- "the weather hasn't been kind to you"
-      i wait for her to say something but she's busy again - "i have so much to do.. why don't you settle here and make my life easier," she says with a forced smile.
-      On the other side of Tawi, past the old bridge, i sit in my memory;
-      perhaps one day i can give her the world, the one she is promised.
-      here on the foothills of the mighty Himalayas, on the other side of the tunnel, i wonder.
-      perhaps i can leave while i still can, younger than i remember, or have i been old and it's merely a dream?
-      have the city swallowed my memories to keep her relevance alive.
-      is she just a figment of her many tangled roads, the tree sitting on the three hills, and disjointed neighborhoods?
-      by the river Tawi - where i once spent the evening swimming in the sweet embrace of liquor, and in bosom of a welcoming morrow
-      overlooking the new bridge, thinking to myself, 'how beautiful is home today'.
-      or making out in the backseat of a confidant's car as we travel through the sidhra road, and she says to me, "do you think this will never end?"
-      and before i can tell her the truth - i see a fleeting glimpse of silver; and there i am -- in tomorrow -- far from the edges of the mighty Himalayas.
-      i take out my phone, i need to see what time it is, and there on the screen, it says it's 32 degrees of summer in jammu, still -- and i burst into tears.
-      On the other side of Tawi, past the old bridge, i am my memory.`,
+    title: "What if it's not a man, it must be a ghost ?",
+    story: `
+    
+home is where the heart is, but what if the heart is broken and lost ?
+
+what then, when there are no roads and no pathways, but a forest with naked trees, and with barely enough sunlight creeping in, to make out the void that surrounds us at all times.
+
+what if a mind does not require a body anymore ?
+
+where do we go from there ?
+
+questions pierce my conscience like an asteroid hitting earth traveling at a thousand miles per heart beat,
+
+evaporating any sense of belief or religion that existed in the deepest corners of my being, resembling a fire that even sun is afraid of --
+
+what if the answers never come ?
+
+what if everything ends before i can wake up, before i have the urge to do something worthwhile with my dreams and fears,
+
+i can build castles in sand and bury my doubts in tiny rooms with tiny beds, but never escape this impending sense of doom that has made a circus in my veins, always to and fro the axis, as i wait for the silence to scream from across the ocean, i guess i'm still waiting for somebody to say my name before i forget how to think,
+
+and i'm still thinking of various ways to end this train of thought and perhaps i'll jump off at the next station, i can see myself from afar howling at the wheels of my suffering for taking a turn for the worse,
+
+it's better if i leave this room before it devours me, i have so much to think and so little room to sit idle, it's as if the walls are suffocating me for fun, every brick vibrating like the bones in my body, trembling in a careless rhythm --
+
+and it feels as if i can never escape from this sadness that has made a nest inside my hollowed body, i am but a step away from breaking down in little brittle pieces of absolute nothing,
+
+i'm so close to being scattered, of crying rivers and oceans of my solitude and misguided birth, but i never do, i never let the rain **** the storm --
+
+i never let the blues paint over the rotten reds, and greens and everything that does not come with a colour,
+
+i enjoy my drakness alone, and i make peace with the ghosts those dance around us when nobody's looking,
+
+i swallow my screams until i'm drowning in my own sorrows, my eyes in a horrific trance, watching the atoms destroy each other a billion times in plain sight,
+
+it kills me that nobody bothers, nobody cares until they're dying, with unrelenting sadness at all times breathing down their necks, ready to bite and drain away the lesser world.
+
+why life when there must've been so much before ? -- i wonder in disguise of madness and tame melancholia, ruined by man made conditions and nefarious activities of the restless and unkept,
+
+and yes, i'm talking about you too, about us, about the gods that live in palaces made of rejected prayers and songs,  
+
+i'm talking about memories, slowly decomposing into dead skin and dusty old book shelves that harbour nothing more than old age and forgotten fingerprints fading away even though the arms of the clocks on the unraveled walls have stopped moving, and the time has stood still peeking from outside the window, waiting for somebody to draw the curtains.
+
+in the cold gloomy room where i've sat everyday for days to come, i sit even now paying attention to every detail, with empty promises and smothered dreams, with voices that echo across the many places inside my mind, buzzing with words that change with every step, and no matter how deep i crawl there'll always be something on the outside that just doesn't make sense.
+
+i wonder if that's how people feel, otherwise it'll be harder for me to explain when i'm done talking,
+
+i'm always breathing the fumes of whispers and stories that people radiate, walking room to room, traveling in circles, and in straight lines that never deviate to accommodate any other shape, reason or thought, always blind to the things passing us by, never turning to see if there's more than what greets the eye when you're looking for something out of place.
+
+perhaps that's why we never leave our souls and wander about in the world of ghosts to see for ourselves if there's more than what we think there is, always believing to choose the lies instead of the truth because we were taught not to be real in this binary world where being out of the box means you're exposed,
+
+that's when i wrestle with the man in the mirror, strangle him and complicate him, abuse him and starve him, carve out his body in my own, paint over him until all that i see, are my eyes peering into my soul, telling my mind that my thoughts have died a sudden death and all there is, is an echo that keeps fading away whenever i remember i do exist, and this is more than just reality, and i'll be better off without my own company,
+
+who am i ? three words that keep me from ending it all, i hope there's no answer.`,
   },
   {
     id: "2",
-    title: "Trusted by Company",
-    story: "230+",
+    title: "Page 667",
+    story: `
+    
+why do men die for other men ?
+what compels them to give up their lives for the lives of their fellow men ?
+
+is it love ? is it duty ? or is it just plain madness ? is it that bond of blood ? or a promise to be better ? or is it simply what being a human is ?
+
+
+the same men capable of destroying a million lives in pursuit of their own ideology ? the same men who for the purpose of their own greed and need can ignore the very definitions of civility and liberty and justice.
+
+
+can we still call them men ?
+
+
+what is happening at this hour in this nation, a nation which is thousands of years old and in making; isn't different from what has happened in the past and unfortunately that is going to happen in the unsuspecting future.
+
+people are turning to an ideology that not only imprisons the free bird in the sky, but also retaliates if it so chooses to lay on a different branch.
+
+diversity isn't celebrated anymore, but rather is frowned upon by the masses, who believe that past holds no relevance over the future.
+
+acceptance, and the very creed upon which the great men who came before us, and made us who we are today - their legacy and wisdom is being demolished, like cards in the winds; and just like the structures of the ancient, for they no longer are painted with the colours we are familiar with today.
+
+
+sheep and wolves alike, are being chased by the blood hounds, cornering every whisper with words of the system, a system that has been diseased from the inside, infecting the very veins of this great nation that has stood the test of age and it's many a poisons for millennia and more.
+
+
+bit by bit the great walls of knowledge and of the enlightened spirits are being razed down by a mere fool in different costumes, performing in a circus build upon the ashes of the innocent and the innocence of the communities that now long for blood.
+
+
+the very nature of this great and grave divide, is unnatural, passed down by the same set of hands that once pulled the chains and carried with them - forcefully, a plight of millions, suppressed and then set aside fanatically, all in the name of a devotional creed.
+
+lizards in boxes pretending to be voices of the free and humane, casting their spells on the fragile and a blind audience, numb by all the back and forth between the gods, and as always, only the peasant suffers.
+
+
+how many more homes must vanish before we realise there's no magic in the disappearing of colours, and the despairing remains of the one's gone, painted across the streets in black and white, begging for somebody to give them their proper funeral.
+
+it is men who take life, animals don't **** for their sins, they never have, for they don't know what it's like to be tamed by fire.
+
+they'd rather burn, than become more like us.
+
+maybe that tells it all, and maybe that is why, the devil may have horns and hoofs, but it never haunts and hunts the wild.
+
+
+we are what we love, but we become who we hate, always - in the end, until something worse comes around to make things better.`,
   },
   {
     id: "3",
-    title: "Transaction",
-    story: "$230M+",
+    title: "A tale like no other",
+    story: `
+    
+bursting through his skin, the insects crawling on the inside found their way to every corner of his soul, and he stood there wrapped in agony of a thousand burning suns, and the moon was ever present as it has ever been.
+
+the battle was lost ages ago but only now the seeds were sprouting from the ends of a forgotten symphony, played by the devil, and groomed by the ills of a broken man.
+
+the light of a thousand burning stars couldn't save him from this darkness, casted by the absence of one mere lonely ball of fire, barely big enough to leave behind a legacy that would survive the approaching end everything there ever was has to bear, and live with.
+
+and in that moment of utter despair and pain, a song was sung, from across the different lands and seven seas, as far as anybody has ever gone, whispered out to the cold by the whisperer, seeking a final good-bye, one last of times, and as many heart beats.
+
+the sound never dies, the swollen winds can find their ways to any who dares to listen, to breathe it in, and swallow it down.
+
+as it did that one night before the spring, at that lonely hour, for the man in the dirt, fighting his brain from exploding.
+
+as he lay there in trance, his face stuck to his knees and arms wrapped in a cloak, to keep the demons away and insects from taking the last of what remains, mumbling to himself broken words left clinging in the deepest corners of his diseased conscience.
+
+at the very end, there's only light, for darkness will lose any meaning, any sight without a spectator, it would cease to exist.
+
+and maybe that was the reason, or maybe it didn't have one, just like a million little tales flowing in every direction, on this excluded part of the universe, in depths of blinding darkness, barely visible to the naked eye.
+
+but whatever it took, the magnificent sun rose as it has done, faithfully, for as long as anything can remember, to feed the tiny little speck of nothing, one more day in the awakening.
+
+the spring had come, and the man was free,
+and all that is left was stardust.
+I tried to explore many themes here, maybe it's just the depression kicking in, but the kind that inspires to be better. Feed the guilt and evil to the paper, ink the words and find solace in corrupting some other mind. `,
   },
 ];
 
@@ -38,284 +141,106 @@ export const poetry = [
   {
 
     id: "1",
-    title: "User Active",
+    title: "Souvenir",
     poem: `
-sound of wilderness
-has come to pass
+i am woe
 
-machines of men
-have come to age  
+her endless
+desolation
 
-children no longer
-go outside
+the last refuge
+of her memory
 
-it is not safe to
-breathe
+of the bitter
+days and sweet
+summers
 
-the traffic is
-too much
+of an autumn
+that sleeps in
+me
 
-and streets
-are all crowded
+and i hear
+her silence
 
-old buses are filled
-with people who do
-not have time to live
+reverberate in
+the abyss of my
+confinement
 
-there are no
-stars in the sky
+but there is no
+escape
 
-the sun is masked
-by the tall buildings
+i am nothing
+without her
 
-water is no
-longer free
-
-fire is now
-expensive
-
-the night is
-never dark
-
-pierced by the
-screams of a thousand
-lights
-
-without hope
-or the warm sun
-
-tired and
-angry
-
-people watch the
-tall buildings
-
-stare them
-down
-
-watch the
-neon signs
-street lights
-
-cars, trees
-and music
-
-pass them
-by
-
-one by
-one
-
-they are
-forgotten
-
-placed inside
-decaying
-
-old crowded
-buses
-
-one by
-one
-
-they become
-so many
-
-a town
-a city
-a slum
-
-that speaks of
-nothing
-
-not a word
-
-only silence and
-more silence
-
-and the silence
-becomes so heavy
-
-crushing dreams
-of every new born
-
-until the silence
-begets a scream
-
-begets a machine
-with a hammer
-
-that knocks
-on their feeble
-doors
-
-flatten their
-denude walls
-
-for opulent men living in
-the silver clouds
-
-in tall buildings with
-neon signs
-
-men who
-own
-
-hope
-
-the sun
-the buildings
-the mountains
-expensive cars
-diamond rings
-salaries
-army
-
-old crowded
-buses
-
-traffic and
-winter smog
-
-birds chirping
-by the windows
-
-voices talking
-in the room
-
-people tired
-and bothered
-
-hunch over in
-their despair
-
-coiled up in
-corners
-
-waiting for
-the batteries
-to run out
-
-suffering in
-silence
-
-telling their
-fractured stories
-
-that speak of
-nothing
-
-not a word
-
-only silence and
-more silence
-
-until the silence
-becomes so heavy
-
-that speaks of
-nothing
-
-not a word
-only silence  
-
-until the silence
-begets a scream
-
-begets a machine
-with a hammer
-
-that knocks on
-feeble doors
-
-flatten the
-rustic walls
-
-to mine the rubble
-and mint more sky for
-opulent men living in
-the silver clouds
-
-men who
-own
-
-hope
-
-the sun
-the river
-the moon
-the mountain
-summer
-spring
-
-golden sunsets
-expensive cars
-exquisite laughter
-
-each worth more
-than a lifetime
-
-of impoverished
-daughters and their
-sons
-
-angry fathers and
-women they beat
-
-mothers and
-whores and
-beggars and
-
-millions upon
-millions
-
-without hope
-or the bright sun
-
-silent as
-a scream
-
-silent as
-a whisper
-
-silent as
-violence
-
-and it speaks
-of nothing
-
-not a word
-
-only silence and
-more silence
-
-passed down
-impoverished
-malnutritioned
-
-millions upon
-millions
-
-such is the
-world
-
-without hope
-or the bright sun
-
-each laugh as expensive
-as an entire lifetime
-
-suffering in
-silence.`,
+and she is
+my dream`,
   },
   {
     id: "2",
-    title: "Trusted by Company",
-    poem: "230+",
+    title: "Crowded",
+    poem:`
+    
+they sought me in whisper
+in colours they could find
+
+they painted of me a picture
+of a man bitter and confined
+
+they found me in winter
+a song frozen in time
+
+they caught me - a sliver
+symphony of the mind
+
+they bought me in silver
+treasure of the divine
+
+they divide me in scripture
+then imprison me in science
+
+they cast from me a river
+of melancholy and wine
+
+they ask of me a mirror
+to show them they're blind`,
   },
   {
     id: "3",
-    title: "Transaction",
-    poem: "$230M+",
+    title: "Panthalassa",
+    poem: `
+
+it is dark as
+it is cold
+
+i am naked/falling into
+another song
+
+caressing me slowly
+are the tides of the
+ocean
+
+it only finds
+me when i am
+scattered
+
+when there is
+nothing/more than
+flesh and bones;
+
+people aren't
+made of silence
+
+captured in
+our skin
+
+there is an
+abyss
+
+pulsating in the
+middle of it all
+
+keeping us
+awake.
+`,
   },
 ];
